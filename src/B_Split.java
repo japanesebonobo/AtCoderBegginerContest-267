837a03530d02bbd7dd9490f0edb8b7c982092716
japanesebonobo <tomi119119@gmail.com>
2022-09-06 00:57:52 +0900

    fix B

--- src/B_Split.java
+++ src/B_Split.java
@@ -17,50 +17,27 @@ public class B_Split {
             }
         }
 
-        List<Integer> single = new ArrayList<Integer>(Arrays.asList(4,6));
-        int[][] pair = {{1,5},{3,9},{2,8}};
-        for (int i = 1; i < flag.length; i++) {
-            if(flag[i] == 0 && single.contains(i)) {
-                if(i == 4 && flag[7] == 1 && (flag[2] == 1 || flag[8] == 1 || flag[5] == 1 || flag[1] == 1 || flag[3] == 1 || flag[9] == 1 || flag[6] == 1 || flag[10] == 1)) {
-                    ans = "Yes";
-                    System.out.println(ans);
-                    System.exit(0);;
-                }
-                if(i == 6 && flag[10] == 1 && (flag[3] == 1 || flag[9] == 1 || flag[5] == 1 || flag[1] == 1 || flag[2] == 1 || flag[8] == 1 || flag[4] == 1 || flag[10] == 1)) {
-                    ans = "Yes";
-                    System.out.println(ans);
-                    System.exit(0);;
-                }
-            }
-            if(flag[i] == 0 && !single.contains(i)) {
-                for (int j = 0; j < pair.length; j++) {
-                    if (flag[pair[j][0]] == 0 && flag[pair[j][1]] == 0) {
-                        if(i == 1 || i == 5) {
-                            if ((flag[4] == 1 || (flag[2] == 1 ||flag[8] == 1)||flag[7] == 1) || ((flag[3] == 1 ||flag[9] == 1) ||flag[6] == 1 ||flag[10] == 1)) {
-                                ans = "Yes";
-                                System.out.println(ans);
-                                System.exit(0);;
-                            }
-                        }
-                        if(i == 3 || i == 9) {
-                            if ((flag[4] == 1 || (flag[2] == 1 ||flag[8] == 1)||flag[7] == 1||(flag[1] ==1 || flag[5] ==1)) || (flag[6] == 1 ||flag[10] == 1)) {
-                                ans = "Yes";
-                                System.out.println(ans);
-                                System.exit(0);;
-                            }
-                        }
-                        if(i == 2 || i == 8) {
-                            if ((flag[4] == 1||flag[7] == 1) || ((flag[5] == 1 ||flag[1] == 1) ||(flag[3] == 1 ||flag[9] == 1)|| flag[6]==1 || flag[10]==1)) {
-                                ans = "Yes";
-                                System.out.println(ans);
-                                System.exit(0);;
-                            }
-                        }
-                    }
-                }
+        int count = 0;
+        char line[] = new char[7];
+        line[0] = '×';
+        
+        line[1] = flag[7] == 1 ? '○' : '×';
+        line[2] = (flag[4] == 1) ? '○' : '×';
+        line[3] = (flag[2] == 1 || flag[8] == 1) ? '○' : '×';
+        line[4] = (flag[1] == 1 || flag[5] == 1) ? '○' : '×';
+        line[5] = (flag[3] == 1 || flag[9] == 1) ? '○' : '×';
+        line[6] = (flag[10] == 1) ? '○' : '×';
+
+        for (int i = 0; i < line.length-1; i++) {
+            if(line[i] == '×' && line[i+1] == '○') {
+                count++;
             }
         }
-    
+
+        if(count > 1) {
+            ans = "Yes";
+        }
+        
         System.out.println(ans);
     }
 }
